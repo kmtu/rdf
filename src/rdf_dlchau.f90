ddb35a48ce3dfca2113cb0e70ea2adda24ecca54
KM Tu <tutu522@gmail.com>
2011-03-19 23:40:54 +0800

    4th

--- src/rdf_dlchau.f90
+++ src/rdf_dlchau.f90
@@ -1,5 +1,5 @@
 !***************************************************************************
-!    This program calculate the radial distribution function from 
+!    This program calculates the radial distribution function from 
 !    Chau-made DL_POLY's HISTORY files.
 !
 !     KM Tu 2011                                                            
@@ -8,33 +8,40 @@
 
 PROGRAM rdf_dlchau
   IMPLICIT NONE
-  CHARACTER(LEN=128) :: input_filename, output_filename
-  INTEGER, PARAMETER :: input_fileid = 11, output_fileid = 12
-  LOGICAL :: is_input_filename_assigned, is_output_filename_assigned
+  CHARACTER(LEN=128) :: output_filename, control_filename, atqref_filename
+  INTEGER, PARAMETER :: output_fileid = 11, control_fileid = 12, atqref_fileid = 13
+  CHARACTER(LEN=128), DIMENSION(:), ALLOCATABLE :: data_filename
+  INTEGER, DIMENSION(:), ALLOCATABLE :: data_fileid
   
-  is_input_filename_assigned = .FALSE.
-  is_output_filename_assigned = .FALSE.
-  
-  call get_argument()  
+  REAL(KIND=8) :: dr !the thickness of each rdf shell
+  LOGICAL :: is_data_filename_assigned, is_atqref_filename_assigned
 
-  if (.NOT. is_output_filename_assigned) then
-     output_filename = TRIM(ADJUSTL(input_filename)) // ".rdf"
-  end if
+  is_data_filename_assigned = .FALSE.
+  is_atqref_filename_assigned = .FALSE.
+  
+  !default values
+  control_filename = "rdf_control"
+  output_filename = "rdf_results"
+  dr = 0.1
+
+  !check ATQREF
+  
+  call get_argument_command()
+  call get_argument_control()
   
 CONTAINS
-  SUBROUTINE get_argument()
+  SUBROUTINE get_argument_command()
     IMPLICIT NONE
-    INTEGER :: stat, i, n
+    INTEGER :: stat, i, num_arg, num_data_files
     INTEGER, PARAMETER :: LEAST_REQUIRED_NUM_ARG = 4
     CHARACTER(LEN=128) :: usage, arg
 
-    n = COMMAND_ARGUMENT_COUNT()
+    num_arg = COMMAND_ARGUMENT_COUNT()
     call GET_COMMAND_ARGUMENT(NUMBER=0, VALUE=arg)
-    !<column>: the column to be used as data.
-    usage = "Usage: " // TRIM(ADJUSTL(arg)) // " -f <in file> -r <dr> &
-         &[-o <out file>]"
+    usage = "Usage: " // TRIM(ADJUSTL(arg)) // " -f <data file1> -a <ATQREF file>&
+         & [<data file2>...] [-c <control file>] [-o <out file>]"
 
-    if (n < LEAST_REQUIRED_NUM_ARG) then
+    if (num_arg < LEAST_REQUIRED_NUM_ARG) then
        write(*,*) "Insufficient arguments!"
        write(*,*) usage
        call EXIT(1)
@@ -46,15 +53,56 @@ CONTAINS
        i = i + 1
        select case (arg)
        case ('-f')
-          call GET_COMMAND_ARGUMENT(NUMBER=i, VALUE=input_filename, STATUS=stat)
-          i = i + 1
-          if (stat /= 0) then
-             write(*,*) "Unable to read the value of argument -f"
+          !count number of data files                                      
+          num_data_files = 0
+          j = i
+          do while (.TRUE.)
+             j = j + 1
+             if (j > num_arg) then
+                EXIT
+             end if
+             call GET_COMMAND_ARGUMENT(NUMBER=j, VALUE=arg, STATUS=stat)
+             if (stat /= 0) then
+                write(*,*) "Error: unable to read the value of argument -f &
+                     &<data file1> [<data file2> ...]"
+                write(*,*) usage
+                call EXIT(1)
+             else if (arg(1:1) == '-') then !end of data file arguments    
+                EXIT
+             end if
+             num_data_files = num_data_files + 1
+          end do
+          if (num_data_files == 0) then
+             write(*,*) "Error: at least one data file must be provided!"
              write(*,*) usage
              call EXIT(1)
           end if
-          is_input_filename_assigned = .TRUE.
+          
+          ALLOCATE(data_fileid(num_data_files))
+          ALLOCATE(data_filename(num_data_files))
+          
+          do j = 1, num_data_files
+             i = i + 1
+             call GET_COMMAND_ARGUMENT(NUMBER=i, VALUE=data_filename(j), STATUS=stat)
+             if (stat /= 0) then
+                write(*,*) "Error: unable to read the value of argument -f &          
+                     &<data file1> [<data file2> ...]"
+                write(*,*) usage
+                call EXIT(1)
+             end if
+             is_data_filename_assigned = .TRUE.
+          end do
 
+       case ('-a')
+          call GET_COMMAND_ARGUMENT(NUMBER=i, VALUE=atqref_filename, STATUS=stat)
+          i = i + 1
+          if (stat /= 0) then
+             write(*,*) "Unable to read the value of argument -a"
+             write(*,*) usage
+             call EXIT(1)
+          end if
+          is_atqref_filename_assigned = .TRUE.
+          
        case ('-o')
           call GET_COMMAND_ARGUMENT(NUMBER=i, VALUE=output_filename, STATUS=stat)
           i = i + 1
@@ -63,25 +111,13 @@ CONTAINS
              write(*,*) usage
              call EXIT(1)
           end if
-          is_output_filename_assigned = .TRUE.
-          
-       case ('-r')
-          call GET_COMMAND_ARGUMENT(NUMBER=i, VALUE=arg, STATUS=stat)
+
+       case ('-c')
+          call GET_COMMAND_ARGUMENT(NUMBER=i, VALUE=control_filename, STATUS=stat)
           i = i + 1
           if (stat /= 0) then
-             write(*,*) "Unable to read the value of argument -r"
+             write(*,*) "Unable to read the value of argument -c"
              write(*,*) usage
-             call EXIT(1)
-          end if
-          read(arg, *, IOSTAT=stat) column
-          if (stat /= 0) then
-             write(*,*) "Unable to parse the value of argument -r, a&
-                  & real number is needed!"
-             write(*,*) usage
-             call EXIT(1)
-          else if (column <= 0) then
-             write(*,*) "Illegal value of argument -r, a positive real&
-                  & number is needed!"
              call EXIT(1)
           end if
           
@@ -91,10 +127,32 @@ CONTAINS
        end select
     end do
 
-    if (.NOT. is_input_filename_assigned) then
-       write(*,*) "<in file> (input filename) should be provided."
+    !check if all the neccessary arguments are given
+    if (.NOT. is_data_filename_assigned) then
+       write(*,*) "At least one <data file> is needed!"
        write(*,*) usage
        call EXIT(1)
+    else if (.NOT. is_atqref_filename_assigned) then
+       write(*,*) "ATQREF file is needed!"
+       write(*,*) usage
+       call EXIT(1)       
     end if
-  END SUBROUTINE get_argument  
+    
+    !open every data file                                        
+    do i = 1, num_data_files
+       data_fileid(i) = 100 + i
+       open(UNIT=data_fileid(i), FILE=data_filename(i), IOSTAT=stat, &
+            &STATUS="OLD", ACTION="READ")
+       if (stat /=0) then
+          write(*,*) "Error: unable to open file: ", TRIM(ADJUSTL(data_filename(i)))
+          call EXIT(1)
+       end if
+       write(*,*) "data file ",i ,":", TRIM(ADJUSTL(data_filename(i)))
+    end do
+  END SUBROUTINE get_argument_command
+
+  SUBROUTINE get_argument_control()
+    IMPLICIT NONE
+    
+  END SUBROUTINE get_argument_control
 END PROGRAM rdf_dlchau
