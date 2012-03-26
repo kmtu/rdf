cf7f02d2a1a46dd2ea0c28d85fec3021f0b24c18
KM Tu <tutu522@gmail.com>
2012-03-26 21:10:38 +0900

    Add dataRange, correlated, beginIndex

--- src/run_rdf.py
+++ src/run_rdf.py
@@ -2,22 +2,38 @@
 import sys
 import argparse
 import subprocess
+import math
 
 parser = argparse.ArgumentParser(description='Process data to RDF')
-parser.add_argument('dataIndexFile', nargs=1, type=argparse.FileType('r'))
-parser.add_argument('window', nargs=1, type=int, help="Window size in ns")
-parser.add_argument('outputBaseName', nargs=1,
-                     help = "output file name = outputBaseName-01,-02,-03...")
+parser.add_argument('dataIndexFile', type=argparse.FileType('r'), metavar='FILE')
+parser.add_argument('window', type=int, help="Window size in ns")
+parser.add_argument('outputBaseName', 
+                     help = "Output file name = outputBaseName-01,-02,-03...")
+parser.add_argument('-r', '--dataRange', nargs=2, type=int, metavar=('BEGIN', 'END'),
+                     help = "The data range to be processed. ex. -r 6 10, means 6ns to 10ns.")
+parser.add_argument('--correlated', action='store_true', default=False,
+                     help = "Process data with the window in a correlated way: 1 2 3, 2 3 4, 3 4 5...\n"+
+                            "Default is False, i.e. uncorrelated: 1 2 3, 4 5 6,...")
+parser.add_argument('--beginIndex', type=int, default=1,
+                     help = "Only takes effect for uncorrelated rdf.")
 args = parser.parse_args()
 
-dataIndexFile = args.dataIndexFile[0]
-window = args.window[0]
+dataIndexFile = args.dataIndexFile
+window = args.window
 control = "../rdf_control"
-outputBaseName = args.outputBaseName[0]
+outputBaseName = args.outputBaseName
+isCorrelated = args.correlated
+beginIndex = args.beginIndex
 
-currentTime = -1
+if args.dataRange == None:
+    dataRange = [0, 0]
+else:
+    dataRange = [i-1 for i in args.dataRange]
+
+currentTime = -1 #begin from 0
 nextTime = 1
 dataFileList = []
+isWithinRange = False
 for (i, line) in enumerate(dataIndexFile):
     line = line.strip()
     if i == 0:
@@ -28,11 +44,16 @@ for (i, line) in enumerate(dataIndexFile):
         try:
             currentTime = int(line) - 1
         except ValueError:
-            dataFileList[currentTime].append(dataDir + "/" + line)
+            if isWithinRange:
+                dataFileList[currentTime - dataRange[0]].append(dataDir + "/" + line)
         else:
             if currentTime == nextTime - 1:
-                dataFileList.append([])
                 nextTime += 1
+                if args.dataRange == None or (currentTime >= dataRange[0] and currentTime <= dataRange[1]):
+                    dataFileList.append([])
+                    isWithinRange = True
+                else:
+                    isWithinRange = False
             else:
                 sys.exit("Data index file error: " +
                          "data time is wrong or discontinued")
@@ -41,12 +62,23 @@ print("Window size: " + str(window))
 for i in dataFileList:
     print(i)
 
-for i in range(len(dataFileList) - window + 1):
-    files = []
-    for j in range(window):
-        files += dataFileList[i+j]
-#    print("rdf_dlchau -c " + control + " -o " + outputBaseName+"-%02d"%i + " -f " + " ".join(files))
-    subprocess.check_call(["rdf_dlchau", "-c", control, 
-                           "-o", outputBaseName + "-%02d"%(i+1),
-                           "-f"] + files)
+
+if isCorrelated:
+    for i in range(len(dataFileList) - window + 1):
+        files = []
+        for j in range(window):
+            files += dataFileList[i+j]
+    #    print("rdf_dlchau -c " + control + " -o " + outputBaseName+"-%02d"%i + " -f " + " ".join(files))
+        subprocess.check_call(["rdf_dlchau", "-c", control, 
+                               "-o", outputBaseName + "-%02d"%(i+dataRange[0]+1),
+                               "-f"] + files)
+else:
+    for i in range(int(math.floor(len(dataFileList) / window))):
+        files = []
+        for j in range(window):
+            files += dataFileList[window*i+j]
+    #    print("rdf_dlchau -c " + control + " -o " + outputBaseName+"-%02d"%i + " -f " + " ".join(files))
+        subprocess.check_call(["rdf_dlchau", "-c", control, 
+                               "-o", outputBaseName + "-%02d"%(i+beginIndex),
+                               "-f"] + files)
 
